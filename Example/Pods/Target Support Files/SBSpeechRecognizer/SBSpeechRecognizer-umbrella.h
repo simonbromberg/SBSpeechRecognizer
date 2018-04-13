72b6b10d3d4df2464eb2730a038914ea4b0c8372
simonbromberg <bshimmy@gmail.com>
2018-04-13 01:01:55 -0400

    Added iOS 10 platform specifier to example's podfile, remove tracking of pods folder

--- Example/Pods/Target Support Files/SBSpeechRecognizer/SBSpeechRecognizer-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double SBSpeechRecognizerVersionNumber;
-FOUNDATION_EXPORT const unsigned char SBSpeechRecognizerVersionString[];
-
